c18461e53584e5215a23c8e64b033ee63412facd
Thabo Vincent Moloi <thabomoloicodes@gmail.com>
2023-10-28 14:49:44 +0200

    Add users namespace

--- apps/server/app/api/__init__.py
+++ apps/server/app/api/__init__.py
@@ -1,6 +1,7 @@
 from app.extensions import api
 from app.api import jwt_callbacks
 from app.api.namespaces.auth import auth_ns
-
+from app.api.namespaces.users import users_ns
 
 api.add_namespace(auth_ns)
+api.add_namespace(users_ns)
---
+++ apps/server/app/api/namespaces/users.py
@@ -0,0 +1,25 @@
+from flask_restx import Namespace, Resource
+
+from app.models import db
+from app.models.users import User
+from app.api.models import user_model
+
+users_ns = Namespace("users", description="Operations for users")
+
+
+@users_ns.route("/users")
+class UsersListResource(Resource):
+    @users_ns.marshal_list_with(user_model, envelope="users")
+    def get(self):
+        return User.query.all()
+
+
+@users_ns.route("/users/<string:id>")
+@users_ns.doc(params={"id": "The unique identifier for the user."})
+class UsersResource(Resource):
+    @users_ns.marshal_with(user_model, envelope="user")
+    def get(self, id: str):
+        user: User = User.query.get(id)
+        if user is None:
+            return {"msg": "The requested user cannot be found."}
+        return user
