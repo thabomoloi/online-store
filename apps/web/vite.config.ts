335ec99528d308a9e49412f470fa2739986bed1c
Thabo Moloi Vincent <thabomoloicodes@gmail.com>
2023-12-10 11:48:08 +0200

    Add server proxy

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -16,4 +16,13 @@ export default defineConfig({
 			"@": path.resolve(__dirname, "./src"),
 		},
 	},
+	server: {
+		proxy: {
+			"/api": {
+				target: "http://127.0.0.1:5000",
+				changeOrigin: false,
+				secure: false,
+			},
+		},
+	},
 });
