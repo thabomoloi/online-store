d52a680ddb10795303eecc2dfa1adbf3dfff5405
Thabo Moloi Vincent <thabomoloicodes@gmail.com>
2023-12-10 22:35:50 +0200

    Add password input component

---
+++ apps/web/src/components/forms/auth/components/PasswordInput.tsx
@@ -0,0 +1,50 @@
+import { Input } from "@/components/ui/input";
+import {
+	Tooltip,
+	TooltipContent,
+	TooltipProvider,
+	TooltipTrigger,
+} from "@/components/ui/tooltip";
+import { Eye, EyeOff } from "lucide-react";
+import { useRef, useState } from "react";
+
+interface PasswordInputProps {
+	field?: any;
+	checkPassword?: (password: string) => void;
+}
+export default function PasswordInput({
+	field,
+	checkPassword,
+}: PasswordInputProps): JSX.Element {
+	const [visible, toggleVisibility] = useState<boolean>(false);
+	const passwordRef = useRef<HTMLInputElement>(null);
+
+	const handleInput = () => {
+		if (passwordRef.current && checkPassword)
+			checkPassword(passwordRef.current.value);
+	};
+
+	return (
+		<div className="flex items-center">
+			<Input
+				{...field}
+				ref={passwordRef}
+				type={visible ? "text" : "password"}
+				autoComplete="current-password"
+				onInput={handleInput}
+			/>
+			<TooltipProvider>
+				<Tooltip>
+					<TooltipTrigger
+						type="button"
+						className="px-2 py-1 -ml-10"
+						onClick={() => toggleVisibility((prev) => !prev)}
+					>
+						{visible ? <EyeOff /> : <Eye />}
+					</TooltipTrigger>
+					<TooltipContent>{visible ? "Hide" : "Show"}</TooltipContent>
+				</Tooltip>
+			</TooltipProvider>
+		</div>
+	);
+}
