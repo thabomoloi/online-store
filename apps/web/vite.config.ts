ec8ff5993ab9a4029e47ef1948b751ee6dd896f2
Thabo Moloi Vincent <thabomoloicodes@gmail.com>
2023-12-12 11:18:37 +0200

    Add proxy for static files

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -23,6 +23,11 @@ export default defineConfig({
 				changeOrigin: false,
 				secure: false,
 			},
+			"/static": {
+				target: "http://127.0.0.1:5000",
+				changeOrigin: false,
+				secure: false,
+			},
 		},
 	},
 });
