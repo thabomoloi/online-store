53d1b4dd98e14687268569d8771e6d3cc2b3885e
Thabo Moloi Vincent <thabomoloicodes@gmail.com>
2023-12-14 01:56:28 +0200

    Format file

--- apps/server/app/api/namespaces/__init__.py
+++ apps/server/app/api/namespaces/__init__.py
@@ -1,12 +1,10 @@
 from typing import Any, Dict
 
 
-def create_response(code: int, description: str, message, data: Any = None) -> Dict[str, Any]:
-    response = {
-        "code": code,
-        "description": description,
-        "message": message
-    }
-    if (data is not None):
+def create_response(
+    code: int, description: str, message, data: Any = None
+) -> Dict[str, Any]:
+    response = {"code": code, "description": description, "message": message}
+    if data is not None:
         response["data"] = data
     return response
