83e3e3cbbf216f75852b1681951f03bf5adabd49
Thabo Moloi Vincent <thabomoloicodes@gmail.com>
2023-12-15 00:22:41 +0200

    Add http response statuses

--- apps/server/app/api/namespaces/auth.py
+++ apps/server/app/api/namespaces/auth.py
@@ -1,4 +1,5 @@
 from datetime import datetime, timezone, timedelta
+from http import HTTPStatus
 from flask_restx import Resource, Namespace
 from flask_jwt_extended import (
     create_access_token,
@@ -19,8 +20,8 @@ from app.api.models.auth_models import (
     is_authenticated_response_model,
     login_model,
     profile_response_model,
-    signup_model, 
-    token_response_model
+    signup_model,
+    token_response_model,
 )
 
 from sqlalchemy.exc import IntegrityError
@@ -34,69 +35,92 @@ auth_ns = Namespace("auth", description="Operations for authentication")
 @auth_ns.route("/signup")
 class SignupResource(Resource):
     @auth_ns.expect(signup_model)
-    @auth_ns.response(code=201, description="Created", model=response_model)
-    @auth_ns.response(code=400, description="Bad request", model=error_model)
-    @auth_ns.response(code=422, description="Unprocessable Entity", model=error_model)
+    @auth_ns.response(
+        code=HTTPStatus.CREATED.value,
+        description=HTTPStatus.CREATED.phrase,
+        model=response_model,
+    )
+    @auth_ns.response(
+        code=HTTPStatus.UNPROCESSABLE_ENTITY.value,
+        description=HTTPStatus.UNPROCESSABLE_ENTITY.phrase,
+        model=error_model,
+    )
+    @auth_ns.response(
+        code=HTTPStatus.INTERNAL_SERVER_ERROR.value,
+        description=HTTPStatus.INTERNAL_SERVER_ERROR.phrase,
+        model=error_model,
+    )
     def post(self):
         """Create new user account"""
         keys = ["first_name", "last_name", "email", "phone", "password"]
-        details = dict(list(map(lambda key: (key, auth_ns.payload.get(key)),keys)))
-        
+        details = dict(list(map(lambda key: (key, auth_ns.payload.get(key)), keys)))
+
         try:
             user = User(**details)
             db.session.add(user)
             db.session.commit()
             response = create_response(
-                code=201, 
-                description="Account created", 
-                message="Your account was created successfully."
+                code=HTTPStatus.CREATED.value,
+                description=HTTPStatus.CREATED.phrase,
+                message="Your account was created successfully.",
             )
         except EmailExistsExcepton:
             db.session.rollback()
             response = create_response(
-                code=400, 
-                description="Bad request", 
-                message="The email address you used already exists. Please login or use a different email address."
-            ) 
+                code=HTTPStatus.BAD_REQUEST.value,
+                description=HTTPStatus.BAD_REQUEST.phrase,
+                message="The email address you used already exists. Please login or use a different email address.",
+            )
         except (IntegrityError, NullPasswordException):
             db.session.rollback()
             response = create_response(
-                code=400, 
-                description="Bad request", 
-                message="Missing some required data."
-            ) 
+                code=HTTPStatus.BAD_REQUEST.value,
+                description=HTTPStatus.BAD_REQUEST.phrase,
+                message="Missing some required data.",
+            )
         except:
             db.session.rollback()
             response = create_response(
-                code=500,
-                description="Internal Server Error",
-                message="Oops! Something went wrong."
+                code=HTTPStatus.INTERNAL_SERVER_ERROR.value,
+                description=HTTPStatus.INTERNAL_SERVER_ERROR.phrase,
+                message="Oops! Something went wrong.",
             )
         finally:
             return response, response["code"]
-    
+
 
 @auth_ns.route("/login")
 class LoginResource(Resource):
     @auth_ns.expect(login_model)
-    @auth_ns.response(code=200, description="Success", model=token_response_model)
-    @auth_ns.response(code=401, description="Unauthorized", model=error_model)
-    @auth_ns.response(code=422, description="Unprocessable Entity", model=error_model)
-    @auth_ns.response(code=500, description="Internal Server Error", model=error_model)
+    @auth_ns.response(
+        code=HTTPStatus.OK.value,
+        description=HTTPStatus.OK.phrase,
+        model=token_response_model,
+    )
+    @auth_ns.response(
+        code=HTTPStatus.UNAUTHORIZED.value,
+        description=HTTPStatus.UNAUTHORIZED.phrase,
+        model=error_model,
+    )
+    @auth_ns.response(
+        code=HTTPStatus.UNPROCESSABLE_ENTITY.value,
+        description=HTTPStatus.UNPROCESSABLE_ENTITY.phrase,
+        model=error_model,
+    )
     def post(self):
         """Log in"""
         email: str = auth_ns.payload.get("email")
         password: str = auth_ns.payload.get("password")
         user: User = User.query.filter_by(email=email).one_or_none()
-        
+
         if not user or not user.verify_password(password):
             response = create_response(
-                code=401,
-                description="Unauthorized",
-                message="Invalid login. Your email or password is incorrect."
+                code=HTTPStatus.UNAUTHORIZED.value,
+                description=HTTPStatus.UNAUTHORIZED.phrase,
+                message="Invalid login. Your email or password is incorrect.",
             )
             return response, response["code"]
-            
+
         refresh_token = create_refresh_token(identity=user)
         refresh_token_jti = decode_token(refresh_token)["jti"]
         access_token = create_access_token(
@@ -104,24 +128,38 @@ class LoginResource(Resource):
             fresh=timedelta(minutes=15),
             additional_claims={"refresh_jti": refresh_token_jti},
         )
-        
+
         response = create_response(
-            code=200,
-            description="Success",
+            code=HTTPStatus.OK.value,
+            description=HTTPStatus.OK.phrase,
             message="Your login was successful.",
-            data=dict(access_token=access_token, refresh_token=refresh_token)
-        ) 
-        
+            data=dict(access_token=access_token, refresh_token=refresh_token),
+        )
+
         return response, response["code"]
 
 
 @auth_ns.route("/logout")
 class LogoutResource(Resource):
     @jwt_required()
-    @auth_ns.response(code=204, description="No Content")
-    @auth_ns.response(code=401, description="Unauthorized", model=error_model)
-    @auth_ns.response(code=422, description="Unprocessable Entity", model=error_model)
-    @auth_ns.response(code=500, description="Internal Server Error", model=error_model)
+    @auth_ns.response(
+        code=HTTPStatus.NO_CONTENT.value, description=HTTPStatus.NO_CONTENT.phrase
+    )
+    @auth_ns.response(
+        code=HTTPStatus.UNAUTHORIZED.value,
+        description=HTTPStatus.UNAUTHORIZED.phrase,
+        model=error_model,
+    )
+    @auth_ns.response(
+        code=HTTPStatus.UNPROCESSABLE_ENTITY.value,
+        description=HTTPStatus.UNPROCESSABLE_ENTITY.phrase,
+        model=error_model,
+    )
+    @auth_ns.response(
+        code=HTTPStatus.INTERNAL_SERVER_ERROR.value,
+        description=HTTPStatus.INTERNAL_SERVER_ERROR.phrase,
+        model=error_model,
+    )
     def delete(self):
         """Log out"""
         token = get_jwt()
@@ -135,9 +173,9 @@ class LogoutResource(Resource):
         except:
             db.session.rollback()
             response = create_response(
-                code=500,
-                description="Internal Server Error",
-                message="Oops! Something went wrong."
+                code=HTTPStatus.INTERNAL_SERVER_ERROR.value,
+                description=HTTPStatus.INTERNAL_SERVER_ERROR.phrase,
+                message="Oops! Something went wrong.",
             )
             return response, response["code"]
 
@@ -145,17 +183,29 @@ class LogoutResource(Resource):
 @auth_ns.route("/refresh")
 class RefreshTokenResource(Resource):
     @jwt_required(refresh=True)
-    @auth_ns.response(code=200, description="Success", model=token_response_model)
-    @auth_ns.response(code=401, description="Unauthorized", model=error_model)
-    @auth_ns.response(code=422, description="Unprocessable Entity", model=error_model)
+    @auth_ns.response(
+        code=HTTPStatus.OK.value,
+        description=HTTPStatus.OK.phrase,
+        model=token_response_model,
+    )
+    @auth_ns.response(
+        code=HTTPStatus.UNAUTHORIZED.value,
+        description=HTTPStatus.UNAUTHORIZED.phrase,
+        model=error_model,
+    )
+    @auth_ns.response(
+        code=HTTPStatus.UNPROCESSABLE_ENTITY.value,
+        description=HTTPStatus.UNPROCESSABLE_ENTITY.phrase,
+        model=error_model,
+    )
     def post(self):
         """Refresh the access token"""
         access_token = create_access_token(identity=get_current_user(), fresh=False)
         response = create_response(
-            code=200,
-            description="Success",
+            code=HTTPStatus.OK.value,
+            description=HTTPStatus.OK.phrase,
             message=None,
-            data=dict(jwt_access_token=access_token)
+            data=dict(jwt_access_token=access_token),
         )
         return response, response["code"]
 
@@ -163,20 +213,32 @@ class RefreshTokenResource(Resource):
 @auth_ns.route("/profile")
 class DetailsResource(Resource):
     @jwt_required()
-    @auth_ns.response(code=200, description="Success", model=profile_response_model)
-    @auth_ns.response(code=401, description="Unauthorized", model=error_model)
-    @auth_ns.response(code=422, description="Unprocessable Entity", model=error_model)
+    @auth_ns.response(
+        code=HTTPStatus.OK.value,
+        description=HTTPStatus.OK.phrase,
+        model=profile_response_model,
+    )
+    @auth_ns.response(
+        code=HTTPStatus.UNAUTHORIZED.value,
+        description=HTTPStatus.UNAUTHORIZED.phrase,
+        model=error_model,
+    )
+    @auth_ns.response(
+        code=HTTPStatus.UNPROCESSABLE_ENTITY.value,
+        description=HTTPStatus.UNPROCESSABLE_ENTITY.phrase,
+        model=error_model,
+    )
     def get(self):
         """Get current user's name and email."""
         user: User = get_current_user()
         response = create_response(
-            code=200,
-            description="Success",
+            code=HTTPStatus.OK.value,
+            description=HTTPStatus.OK.phrase,
             message=None,
             data=dict(
                 name=f"{user.first_name.capitalize()} {user.last_name.capitalize()}",
                 email=user.email,
-            )
+            ),
         )
         return response, response["code"]
 
@@ -184,17 +246,23 @@ class DetailsResource(Resource):
 @auth_ns.route("/is-authenticated")
 class IsAuthenticatedResource(Resource):
     @jwt_required(optional=True)
-    @auth_ns.response(code=200, description="Success", model=is_authenticated_response_model)
-    @auth_ns.response(code=422, description="Unprocessable Entity", model=error_model)
+    @auth_ns.response(
+        code=HTTPStatus.OK.value,
+        description=HTTPStatus.OK.phrase,
+        model=is_authenticated_response_model,
+    )
+    @auth_ns.response(
+        code=HTTPStatus.UNPROCESSABLE_ENTITY.value,
+        description=HTTPStatus.UNPROCESSABLE_ENTITY.phrase,
+        model=error_model,
+    )
     def get(self):
         """Check if the current user is authenticated."""
         user: User = get_current_user()
         response = create_response(
-            code=200,
-            description="Success",
+            code=HTTPStatus.OK.value,
+            description=HTTPStatus.OK.phrase,
             message=None,
-            data=dict(is_authenticated=(user is not None))
+            data=dict(is_authenticated=(user is not None)),
         )
         return response, response["code"]
-    
-    
--- apps/server/app/api/namespaces/users.py
+++ apps/server/app/api/namespaces/users.py
@@ -1,25 +1,92 @@
-from flask_restx import Namespace, Resource
+from http import HTTPStatus
+from flask_jwt_extended import jwt_required
+from flask_restx import Namespace, Resource, marshal
 
 from app.models import db
 from app.models.users import User
-from app.api.models.user_models import user_model
+from app.api.models.base_models import error_model
+from app.api.models.user_models import (
+    user_model,
+    user_response_model,
+    user_list_response_model,
+)
+from app.api.namespaces import create_response
 
 users_ns = Namespace("users", description="Operations for users")
 
 
 @users_ns.route("/users")
 class UsersListResource(Resource):
-    @users_ns.marshal_list_with(user_model, envelope="users")
+    @jwt_required()
+    @users_ns.response(
+        code=HTTPStatus.OK.value,
+        description=HTTPStatus.OK.phrase,
+        model=user_list_response_model,
+    )
+    @users_ns.response(
+        code=HTTPStatus.UNAUTHORIZED.value,
+        description=HTTPStatus.UNAUTHORIZED.phrase,
+        model=error_model,
+    )
+    @users_ns.response(
+        code=HTTPStatus.NOT_FOUND.value,
+        description=HTTPStatus.NOT_FOUND.phrase,
+        model=error_model,
+    )
+    @users_ns.response(
+        code=HTTPStatus.UNPROCESSABLE_ENTITY.value,
+        description=HTTPStatus.UNPROCESSABLE_ENTITY.phrase,
+        model=error_model,
+    )
     def get(self):
-        return User.query.all()
+        response = create_response(
+            code=200,
+            description="Success",
+            message=None,
+            data=marshal(User.query.all(), user_model),
+        )
+        return response, response["code"]
 
 
 @users_ns.route("/users/<string:id>")
 @users_ns.doc(params={"id": "The unique identifier for the user."})
 class UsersResource(Resource):
-    @users_ns.marshal_with(user_model, envelope="user")
+    @jwt_required()
+    @users_ns.response(
+        code=HTTPStatus.OK.value,
+        description=HTTPStatus.OK.phrase,
+        model=user_response_model,
+    )
+    @users_ns.response(
+        code=HTTPStatus.UNAUTHORIZED.value,
+        description=HTTPStatus.UNAUTHORIZED.phrase,
+        model=error_model,
+    )
+    @users_ns.response(
+        code=HTTPStatus.NOT_FOUND.value,
+        description=HTTPStatus.NOT_FOUND.phrase,
+        model=error_model,
+    )
+    @users_ns.response(
+        code=HTTPStatus.UNPROCESSABLE_ENTITY.value,
+        description=HTTPStatus.UNPROCESSABLE_ENTITY.phrase,
+        model=error_model,
+    )
     def get(self, id: str):
         user: User = User.query.get(id)
         if user is None:
-            return {"msg": "The requested user cannot be found."}
-        return user
+            status = HTTPStatus.NOT_FOUND
+            response = create_response(
+                code=status.value,
+                description=status.phrase,
+                message="The requested user cannot be found.",
+            )
+        else:
+            status = HTTPStatus.OK
+            response = create_response(
+                code=status.value,
+                description=status.phrase,
+                message=None,
+                data=marshal(user, user_model),
+            )
+        return response, response["code"]
