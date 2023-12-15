b2cad1d384a56e37ecbfa320ab9bc45ada2f675e
Thabo Moloi Vincent <thabomoloicodes@gmail.com>
2023-12-15 21:07:51 +0200

    Add products namespace to api

--- apps/server/app/api/__init__.py
+++ apps/server/app/api/__init__.py
@@ -2,6 +2,9 @@ from app.extensions import api
 from app.api import jwt_callbacks
 from app.api.namespaces.auth import auth_ns
 from app.api.namespaces.users import users_ns
+from app.api.namespaces.products import products_ns
+
 
 api.add_namespace(auth_ns)
 api.add_namespace(users_ns)
+api.add_namespace(products_ns)
