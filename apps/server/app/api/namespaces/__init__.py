19aa8e2d7c41b12686a7c120e461b8c8581a8166
Thabo Moloi Vincent <thabomoloicodes@gmail.com>
2023-12-16 12:32:04 +0200

    Create a list of error responses from namespace

--- apps/server/app/api/namespaces/__init__.py
+++ apps/server/app/api/namespaces/__init__.py
@@ -1,4 +1,9 @@
+from http import HTTPStatus
 from typing import Any, Dict
+
+from flask_restx import Namespace
+
+from app.api.models.base_models import error_model
 
 
 def create_response(
@@ -8,3 +13,22 @@ def create_response(
     if data is not None:
         response["data"] = data
     return response
+
+
+def make_error_response(namespace: Namespace, status=HTTPStatus.INTERNAL_SERVER_ERROR):
+    return namespace.response(
+        code=status.value, description=status.phrase, model=error_model
+    )
+
+
+def create_error_responses(namespace: Namespace):
+    http_error_statuses = [
+        HTTPStatus.BAD_REQUEST,
+        HTTPStatus.UNAUTHORIZED,
+        HTTPStatus.NOT_FOUND,
+        HTTPStatus.UNPROCESSABLE_ENTITY,
+        HTTPStatus.INTERNAL_SERVER_ERROR,
+    ]
+    return list(
+        map(lambda status: make_error_response(namespace, status), http_error_statuses)
+    )
