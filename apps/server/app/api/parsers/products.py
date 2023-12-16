cf371a66b980a80ff50195a014aaa18f5c84df50
Thabo Moloi Vincent <thabomoloicodes@gmail.com>
2023-12-16 13:03:01 +0200

    Change locations of arguments to form

--- apps/server/app/api/parsers/products.py
+++ apps/server/app/api/parsers/products.py
@@ -3,15 +3,15 @@ from flask_restx import reqparse
 from werkzeug.datastructures import FileStorage
 
 product_parser = reqparse.RequestParser()
-product_parser.add_argument(name="code", required=True, type=str, location="files")
-product_parser.add_argument(name="name", required=True, type=str, location="files")
+product_parser.add_argument(name="code", required=True, type=str, location="form")
+product_parser.add_argument(name="name", required=True, type=str, location="form")
 product_parser.add_argument(
     name="image", required=True, type=FileStorage, location="files"
 )
 product_parser.add_argument(
-    name="description", required=True, type=str, location="files"
+    name="description", required=True, type=str, location="form"
 )
-product_parser.add_argument(name="price", required=True, type=int, location="files")
-product_parser.add_argument(name="stock", required=True, type=int, location="files")
+product_parser.add_argument(name="price", required=True, type=int, location="form")
+product_parser.add_argument(name="stock", required=True, type=int, location="form")
 
 # reqparse.Argument()
